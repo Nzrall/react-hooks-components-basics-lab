2b388733de81ed64cabb85f0a49dfd1603ff998f
Joshua <wilson.joshua@outlook.com>
2021-11-01 15:41:48 -0700

    commit tree ab0764283ceb8a7e0aba9c6048578067ecbb4186

--- src/components/App.js
+++ src/components/App.js
@@ -16,17 +16,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>This is my About section</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
 
 export default App;
+
